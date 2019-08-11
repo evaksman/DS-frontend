ee738235d9d58888efdfd1d7a6f7b539f5e43689
evaksman <waxkat@yandex.ru>
2019-08-11 18:41:18 +0500

    v8. AJAX-отправка формы + некоторые события

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('form').submit(function(event) {
+        //event.preventDefault(); // отменяем отправку
+        $.ajax({
+            url: 'https://digital-spectr.com/ac/academy.php',
+            method: 'post',
+            dataType: 'html',
+            data: $(this).serialize(),
+            success: function (data) {
+                alert(data);
+            },
+            error: function(data) {
+                alert(data);
+            }
+        });
+    })
+});
+
+
+
