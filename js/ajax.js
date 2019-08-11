d65aec4d46ae1ec888b37def2b62d96107491f9f
evaksman <waxkat@yandex.ru>
2019-08-11 21:18:45 +0500

    v11. Правки ajax (предыдущим коммитом потерла лишнее)

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    $('form').submit(function(event) {
+    $('form').submit(function(e) {
+        e.preventDefault();
         $.ajax({
             url: 'https://digital-spectr.com/ac/academy.php',
             method: 'post',
             dataType: 'html',
             data: $(this).serialize(),
             success: function (data) {
-                alert('ОК');
+                alert(data);
             },
             error: function (data) {
-                alert('НЕ ОК');
+                alert(data);
             }
         });
     })
